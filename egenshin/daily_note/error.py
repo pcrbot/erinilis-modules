7b478c05de2dc0b44030bfbb13b1e06c5eed296e
erinilis <389897773@qq.com>
2021-10-17 12:46:59 +0800

    修改获取的js代码

--- egenshin/daily_note/error.py
+++ egenshin/daily_note/error.py
@@ -8,11 +8,13 @@ class Error_Message(Exception):
 class Cookie_Error(Error_Message):
     def __repr__(self):
         t = '''
+* 注意, 这个插件需要获取账号的令牌, 谨慎授权
+
 1. 打开米游社(https://bbs.mihoyo.com/ys/)
 2. 登录游戏账号
 3. F12打开控制台
 4. 输入以下代码运行
-javascript:(()=>{_=(n)=>document.cookie.match(`[;\s+]?${n}=([^;]*)`)?.pop();alert(_("account_id")+","+_("cookie_token"))})();
+javascript:(()=>{_=(name)=>{for(i in(r=document.cookie.split("; "))){arr=r[i].split("=");if(arr[0]==name)return arr[1]}};alert(_("account_id")+","+_("cookie_token"))})();
 5. 复制提示的内容, 私聊发给机器人
 私聊格式为
 
