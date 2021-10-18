7fceb39a6368160427ed739165c0a86cd5b82429
erinilis <389897773@qq.com>
2021-10-18 10:47:01 +0800

    优化复制cookie的代码

--- egenshin/daily_note/error.py
+++ egenshin/daily_note/error.py
@@ -14,13 +14,14 @@ class Cookie_Error(Error_Message):
 2. 登录游戏账号
 3. F12打开控制台
 4. 输入以下代码运行
-javascript:(()=>{_=(name)=>{for(i in(r=document.cookie.split("; "))){arr=r[i].split("=");if(arr[0]==name)return arr[1]}};alert(_("account_id")+","+_("cookie_token"))})();
+javascript:(()=>{_=(n)=>{for(i in(r=document.cookie.split(";"))){var arr=r[i].split("=");if(arr[0].trim()==n)return arr[1];}};c=_("cookie_token")||alert('请重新登录');m=_("account_id")+","+c;c&&confirm('确定复制到剪切板?\n'+m)&&copy(m)})();
 5. 复制提示的内容, 私聊发给机器人
 私聊格式为
 
-原神便笺绑定xxxxxxxxxxxxxx
+yss绑定0000000,xxxxxxxxxxxx
 
-其中xxxxxxxxxxxxxx是复制的内容
+其中0000000,xxxxxxxxxxxx是复制的内容
+*手机需要把这段代码添加到收藏夹 在米游社页面打开
         '''
         return t.rstrip()
 
